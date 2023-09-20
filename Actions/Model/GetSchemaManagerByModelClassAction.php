935f1de47bc470de164f94a9a302195f014a5f44
marco76tv <marco.sottana@gmail.com>
2023-09-20 16:47:06 +0200

    .

--- Actions/Model/GetSchemaManagerByModelClassAction.php
+++ Actions/Model/GetSchemaManagerByModelClassAction.php
@@ -16,22 +16,7 @@ class GetSchemaManagerByModelClassAction
     public function execute(string $modelClass)
     {
         $model=app($modelClass);
-
-        $connectionName = $model->getConnectionName();
-
-        
-        $a= Schema::connection($connectionName); //Illuminate\Database\Schema\MySqlBuilder 
-
-
-        $b=($model->getConnection()); //Illuminate\Database\MySqlConnection 
-        dddx(['a'=>$a->getConnection()
-        ->getDoctrineSchemaManager()
-        ,'b'=>$b->getDoctrineSchemaManager()]);
-        /*
-        $table=$model->getTable();
-        $schemaManager=app(GetSchemaManagerByModelClassAction::class)->execute($modelClass);
-        $indexes= $schemaManager->listTableIndexes($table);
-        return $indexes;
-        */
+        $connection=$model->getConnection();
+        return $connection->getDoctrineSchemaManager();
     }
 }
