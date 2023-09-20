5f12037292f2db46ed934bfd1f1bb519f40612aa
marco xot <marco.sottana@gmail.com>
2023-09-20 13:15:34 +0200

    up

--- Actions/Filament/GetModulesNavigationItems.php
+++ Actions/Filament/GetModulesNavigationItems.php
@@ -4,36 +4,47 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Actions\Filament;
 
-use Illuminate\Support\Str;
 use Filament\Facades\Filament;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\File;
 use Filament\Navigation\NavigationItem;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Modules\Tenant\Services\TenantService;
 use Spatie\QueueableAction\QueueableAction;
 
-class GetModulesNavigationItems
-{
+class GetModulesNavigationItems {
     use QueueableAction;
 
     /**
      * Undocumented function.
      */
-    public function execute():array
-    {
+    public function execute(): array {
         $navs = [];
         $modules = TenantService::allModules(); // app('modules') da errore su container Cache
         foreach ($modules as $module) {
+            // if (! Filament::auth()->check()) {
+            //    continue;
+            // }
             $module_low = Str::lower($module);
+            // Target class [hash] does not exist.
+            // if (! auth()->user()->can('module_'.$module_low)) {
+            //    continue;
+            // }
+            // dddx(Auth::id());
+
             $config = File::getRequire(base_path('Modules/'.$module.'/Config/config.php'));
             $icon = $config['icon'] ?? 'heroicon-o-question-mark-circle';
 
-            $navs[] = NavigationItem::make($module)
+            $nav = NavigationItem::make($module)
                     ->url('/'.$module_low.'/admin')
                     ->icon($icon)
                     ->group('Modules')
-                    ->sort(3);
+                    ->sort(3)
+                    ->visible(fn () => Filament::auth()->user()->hasRole($module_low.'::admin'));
+
+            $navs[] = $nav;
         }
+
         return $navs;
     }
 }
