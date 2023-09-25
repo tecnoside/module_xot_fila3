599c83a49dc5d7293163134d0113154368c83a1a
marco76tv <marco.sottana@gmail.com>
2023-09-25 14:58:33 +0200

    up

---
+++ Datas/JsonResponseData.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Datas;
+
+use Spatie\LaravelData\Data;
+
+/**
+ * Undocumented class.
+ */
+class JsonResponseData extends Data
+{
+    public bool $success = true; // => false,
+    public string $message;
+    public ?int $code = null;
+    public array $data = [];
+    public int $status = 200;
+
+    /*
+    public function toResponse($request)
+    {
+
+    }
+    */
+    public function response()
+    {
+        $data = [
+            'success' => $this->success,
+            'message' => $this->message,
+            'code' => $this->code,
+            'data' => $this->data,
+        ];
+
+        return response()->json($data, $this->status);
+    }
+}
