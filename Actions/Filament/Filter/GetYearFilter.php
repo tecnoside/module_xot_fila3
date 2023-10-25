d990ce9316f9a50d9de16fd03602231627310e6a
marco xot <marco.sottana@gmail.com>
2023-10-25 14:05:43 +0200

    up

---
+++ Actions/Filament/Filter/GetYearFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions\Filament\Filter;
+
+use Filament\Facades\Filament;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Spatie\QueueableAction\QueueableAction;
+use Filament\Tables\Filters\SelectFilter;
+
+class GetYearFilter {
+    use QueueableAction;
+
+    /**
+     * Undocumented function.
+     */
+    public function execute(string $fieldName,int $from,int $to): SelectFilter {
+        $opts=[];
+        for($curr=$from;$curr<=$to;$curr++){
+            $opts[$curr]=$curr;
+        }
+        $res=SelectFilter::make($fieldName)
+            ->options($opts);
+        return $res;
+    }
+}
