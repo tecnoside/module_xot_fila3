4708d5ce0d6c39edcb558eeb0d73dbabab76ae8f
marco76tv <marco.sottana@gmail.com>
2023-11-06 09:42:12 +0100

    .

--- Services/bashscripts/composer_init.sh
+++ Services/bashscripts/composer_init.sh
@@ -7,7 +7,7 @@ rm composer.lock
 rm package-lock.json
 #php -d memory_limit=-1 composer.phar require -W kriswallsmith/assetic
 #php -d memory_limit=-1 composer.phar require -W leafo/lessphp
-#php -d memory_limit=-1 composer.phar require -W leafo/scssphp
+#php -d memory_limit=-1 composer.phar require -W scssphp/scssphp
 #php -d memory_limit=-1 composer.phar require -W lmammino/jsmin4assetic
 #php -d memory_limit=-1 composer.phar require -W maatwebsite/excel
 #php -d memory_limit=-1 composer.phar require -W mrclay/jsmin-php
@@ -26,13 +26,13 @@ php -d memory_limit=-1 composer.phar require -W spipu/html2pdf
 php -d memory_limit=-1 composer.phar require -W psr/simple-cache
 php -d memory_limit=-1 composer.phar require -W kriswallsmith/assetic
 php -d memory_limit=-1 composer.phar require -W leafo/lessphp
-php -d memory_limit=-1 composer.phar require -W leafo/scssphp
+php -d memory_limit=-1 composer.phar require -W scssphp/scssphp
 php -d memory_limit=-1 composer.phar require -W lmammino/jsmin4assetic
 php -d memory_limit=-1 composer.phar require -W maatwebsite/excel
 php -d memory_limit=-1 composer.phar require -W mrclay/jsmin-php
 php -d memory_limit=-1 composer.phar require -W mrclay/minify
 php -d memory_limit=-1 composer.phar require -W natxet/cssmin
-php -d memory_limit=-1 composer.phar require -W patchwork/jsqueeze
+#php -d memory_limit=-1 composer.phar require -W patchwork/jsqueeze
 php -d memory_limit=-1 composer.phar require -W phpoffice/phpspreadsheet
 php -d memory_limit=-1 composer.phar require -W phpoffice/phpword
 php -d memory_limit=-1 composer.phar require -W ptachoire/cssembed
