cfb25921c59bc37d169c5905db852c04c82fb802
marco76tv <marco.sottana@gmail.com>
2023-11-13 16:16:09 +0100

    up

--- Filament/Resources/XotBaseResource.php
+++ Filament/Resources/XotBaseResource.php
@@ -49,6 +49,11 @@ abstract class XotBaseResource extends Resource
         return $res;
     }
 
+    public static function getModelLabel(): string
+    {
+        return static::trans('navigation.name');
+    }
+
     public static function getPluralModelLabel(): string
     {
         return static::trans('navigation.plural');
@@ -59,6 +64,16 @@ abstract class XotBaseResource extends Resource
         return static::trans('navigation.name');
         // return static::trans('navigation.plural');
     }
+
+    // public static function getNavigationIcon(): ?string
+    // {
+    //    return 'heroicon-o-user-group';
+    // }
+
+    // public static function getNavigationSort(): ?int
+    // {
+    //    return 2;
+    // }
 
     public static function getNavigationGroup(): string
     {
