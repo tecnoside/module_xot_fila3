ee90101cd72b400b7cd249bf12801dd3c27ffee4
marco xot <marco.sottana@gmail.com>
2023-11-15 14:01:51 +0100

    up

--- Models/Cache.php
+++ Models/Cache.php
@@ -7,11 +7,16 @@ namespace Modules\Xot\Models;
 /**
  * Modules\Xot\Models\Cache.
  *
+ * @property string $key
+ * @property string $value
+ * @property int $expiration
  * @method static \Modules\Xot\Database\Factories\CacheFactory factory($count = null, $state = [])
- * @method static \Illuminate\Database\Eloquent\Builder|Cache  newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Cache  newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Cache  query()
- *
+ * @method static \Illuminate\Database\Eloquent\Builder|Cache newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Cache newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Cache query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Cache whereExpiration($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cache whereKey($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Cache whereValue($value)
  * @mixin \Eloquent
  */
 class Cache extends BaseModel
--- Models/Feed.php
+++ Models/Feed.php
@@ -11,22 +11,16 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * Modules\Xot\Models\Feed.
  *
- * @property int                             $id
- * @property string|null                     $created_by
- * @property string|null                     $updated_by
+ * @property int $post_id
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- *
  * @method static \Modules\Xot\Database\Factories\FeedFactory factory($count = null, $state = [])
- * @method static Builder|Feed                                newModelQuery()
- * @method static Builder|Feed                                newQuery()
- * @method static Builder|Feed                                query()
- * @method static Builder|Feed                                whereCreatedAt($value)
- * @method static Builder|Feed                                whereCreatedBy($value)
- * @method static Builder|Feed                                whereId($value)
- * @method static Builder|Feed                                whereUpdatedAt($value)
- * @method static Builder|Feed                                whereUpdatedBy($value)
- *
+ * @method static Builder|Feed newModelQuery()
+ * @method static Builder|Feed newQuery()
+ * @method static Builder|Feed query()
+ * @method static Builder|Feed whereCreatedAt($value)
+ * @method static Builder|Feed wherePostId($value)
+ * @method static Builder|Feed whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Feed extends BaseModel
