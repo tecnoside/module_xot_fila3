5f50c553fe593067929cc6131a6462de554309de
marco76tv <marco76tv@users.noreply.github.com>
2023-11-23 11:21:42 +0000

    Check & fix styling

--- Models/Session.php
+++ Models/Session.php
@@ -9,22 +9,24 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * Modules\Xot\Models\Session.
  *
- * @property int $id
- * @property int|null $user_id
+ * @property int         $id
+ * @property int|null    $user_id
  * @property string|null $ip_address
  * @property string|null $user_agent
- * @property string $payload
- * @property int $last_activity
+ * @property string      $payload
+ * @property int         $last_activity
+ *
  * @method static \Modules\Xot\Database\Factories\SessionFactory factory($count = null, $state = [])
- * @method static Builder|Session newModelQuery()
- * @method static Builder|Session newQuery()
- * @method static Builder|Session query()
- * @method static Builder|Session whereId($value)
- * @method static Builder|Session whereIpAddress($value)
- * @method static Builder|Session whereLastActivity($value)
- * @method static Builder|Session wherePayload($value)
- * @method static Builder|Session whereUserAgent($value)
- * @method static Builder|Session whereUserId($value)
+ * @method static Builder|Session                                newModelQuery()
+ * @method static Builder|Session                                newQuery()
+ * @method static Builder|Session                                query()
+ * @method static Builder|Session                                whereId($value)
+ * @method static Builder|Session                                whereIpAddress($value)
+ * @method static Builder|Session                                whereLastActivity($value)
+ * @method static Builder|Session                                wherePayload($value)
+ * @method static Builder|Session                                whereUserAgent($value)
+ * @method static Builder|Session                                whereUserId($value)
+ *
  * @mixin \Eloquent
  */
 class Session extends BaseModel
