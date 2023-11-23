410411ef102e025026394db1601222b951630d45
Malebestia <vair81@gmail.com>
2023-11-23 12:15:18 +0100

    up

--- Models/Feed.php
+++ Models/Feed.php
@@ -11,22 +11,20 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * Modules\Xot\Models\Feed.
  *
- * @property int                             $id
- * @property string|null                     $created_by
- * @property string|null                     $updated_by
+ * @property int $id
+ * @property string|null $created_by
+ * @property string|null $updated_by
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- *
  * @method static \Modules\Xot\Database\Factories\FeedFactory factory($count = null, $state = [])
- * @method static Builder|Feed                                newModelQuery()
- * @method static Builder|Feed                                newQuery()
- * @method static Builder|Feed                                query()
- * @method static Builder|Feed                                whereCreatedAt($value)
- * @method static Builder|Feed                                whereCreatedBy($value)
- * @method static Builder|Feed                                whereId($value)
- * @method static Builder|Feed                                whereUpdatedAt($value)
- * @method static Builder|Feed                                whereUpdatedBy($value)
- *
+ * @method static Builder|Feed newModelQuery()
+ * @method static Builder|Feed newQuery()
+ * @method static Builder|Feed query()
+ * @method static Builder|Feed whereCreatedAt($value)
+ * @method static Builder|Feed whereCreatedBy($value)
+ * @method static Builder|Feed whereId($value)
+ * @method static Builder|Feed whereUpdatedAt($value)
+ * @method static Builder|Feed whereUpdatedBy($value)
  * @mixin \Eloquent
  */
 class Feed extends BaseModel
