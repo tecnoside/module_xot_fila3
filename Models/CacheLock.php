03b0158eff3cab76a897e473c44572ade0d82eee
marco76tv <marco.sottana@gmail.com>
2023-11-27 16:19:30 +0100

    Update model annotations- Refactor model annotations in Cache.php, CacheLock.php, Feed.php, Profile.php, Session.php, and Settings.php.- Remove unnecessary properties and methods.- Improve code formatting and consistency

--- Models/CacheLock.php
+++ Models/CacheLock.php
@@ -9,11 +9,16 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * Modules\Xot\Models\CacheLock.
  *
+ * @property string $key
+ * @property string $owner
+ * @property int $expiration
  * @method static \Modules\Xot\Database\Factories\CacheLockFactory factory($count = null, $state = [])
- * @method static Builder|CacheLock                                newModelQuery()
- * @method static Builder|CacheLock                                newQuery()
- * @method static Builder|CacheLock                                query()
- *
+ * @method static Builder|CacheLock newModelQuery()
+ * @method static Builder|CacheLock newQuery()
+ * @method static Builder|CacheLock query()
+ * @method static Builder|CacheLock whereExpiration($value)
+ * @method static Builder|CacheLock whereKey($value)
+ * @method static Builder|CacheLock whereOwner($value)
  * @mixin \Eloquent
  */
 class CacheLock extends BaseModel
