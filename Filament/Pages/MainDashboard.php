f69ed9258f61c7f2538892b99f19a5267ddb3db9
marco76tv <marco.sottana@gmail.com>
2023-12-04 18:48:27 +0100

    redirect for who has only a module

---
+++ Filament/Pages/MainDashboard.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Filament\Pages;
+
+use Filament\Pages\Page;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+use Filament\Panel;
+
+
+
+class MainDashboard extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-home';
+
+    protected static string $view = 'xot::filament.pages.dashboard';
+
+
+    /*
+    public function __construct(){
+        dddx('a'); //1
+    }
+    */
+
+   
+    public function mount(){
+        $user = auth()->user();
+        $modules=($user->roles->filter(function($item){
+            return Str::endsWith($item->name, '::admin');
+        }));
+
+        if($modules->count()==1){
+            $panel_name=$modules->first()->name;
+            $module_name=Str::before($panel_name, '::admin');
+            $url='/'.$module_name.'/admin/dashboard';
+            redirect($url);
+        }
+
+        /*
+        if (! $user?->hasRole('super-admin')) {
+            redirect('/admin');
+        }
+        */
+    }
+   
+
+    
+    
+}
