3194a325e15415e6e3fa1ec8e75d6c2913dc50ce
marco76tv <marco.sottana@gmail.com>
2023-12-18 16:06:53 +0100

    Fix styling issues in ExportButton, ImportButton, and XotBaseListRecords

--- Actions/Filament/Actions/ExportButton.php
+++ Actions/Filament/Actions/ExportButton.php
@@ -7,34 +7,18 @@ namespace Modules\Xot\Actions\Filament\Actions;
 use Filament\Actions\Action;
 use Spatie\QueueableAction\QueueableAction;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 class ExportButton
 {
     use QueueableAction;
 
     public function execute(): Action
     {
-=======
-class ExportButton {
-    use QueueableAction;
-
-    public function execute(): Action {
->>>>>>> 7301add (.)
-=======
-class ExportButton
-{
-    use QueueableAction;
-
-    public function execute(): Action
-    {
->>>>>>> 583b628 (Check & fix styling)
         return Action::make('export')
             ->label('')
             ->tooltip('export XLS')
             ->icon('heroicon-o-inbox-arrow-down')
             // ->visible(null != $year)
-            ->action(fn () => dddx('WIP'))
+            ->action(static fn () => dddx('WIP'))
         ;
     }
 }
--- Actions/Filament/Actions/ImportButton.php
+++ Actions/Filament/Actions/ImportButton.php
@@ -7,34 +7,18 @@ namespace Modules\Xot\Actions\Filament\Actions;
 use Filament\Actions\Action;
 use Spatie\QueueableAction\QueueableAction;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 class ImportButton
 {
     use QueueableAction;
 
     public function execute(): Action
     {
-=======
-class ImportButton {
-    use QueueableAction;
-
-    public function execute(): Action {
->>>>>>> 7301add (.)
-=======
-class ImportButton
-{
-    use QueueableAction;
-
-    public function execute(): Action
-    {
->>>>>>> 583b628 (Check & fix styling)
         return Action::make('import')
             ->label('')
             ->tooltip('import XLS')
             ->icon('heroicon-o-arrow-up-on-square')
             // ->visible(null != $year)
-            ->action(fn () => dddx('WIP'))
+            ->action(static fn () => dddx('WIP'))
         ;
     }
 }
