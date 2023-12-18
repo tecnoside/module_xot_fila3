cb65357de72fa3e763a44408ab8a19fb07a1870b
marco76tv <marco.sottana@gmail.com>
2023-12-18 15:25:01 +0100

    Refactor error logging and formatting code

--- Filament/Pages/MainDashboard.php
+++ Filament/Pages/MainDashboard.php
@@ -5,17 +5,8 @@ declare(strict_types=1);
 namespace Modules\Xot\Filament\Pages;
 
 use Filament\Pages\Page;
-<<<<<<< HEAD
 use Illuminate\Support\Str;
 use Webmozart\Assert\Assert;
-=======
-use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
-use Filament\Panel;
-
-
->>>>>>> c605e84 (redirect for who has only a module)
 
 class MainDashboard extends Page
 {
@@ -23,42 +14,24 @@ class MainDashboard extends Page
 
     protected static string $view = 'xot::filament.pages.dashboard';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> c605e84 (redirect for who has only a module)
     /*
     public function __construct(){
         dddx('a'); //1
     }
     */
 
-<<<<<<< HEAD
     public function mount(): void
     {
         Assert::notNull($user = auth()->user());
-        $modules = $user->roles->filter(function ($item) {
+        $modules = $user->roles->filter(static function ($item) {
             return Str::endsWith($item->name, '::admin');
         });
 
-        if (1 == $modules->count()) {
+        if (1 === $modules->count()) {
             Assert::notNull($modules->first());
             $panel_name = $modules->first()->name;
             $module_name = Str::before($panel_name, '::admin');
             $url = '/'.$module_name.'/admin';
-=======
-   
-    public function mount(){
-        $user = auth()->user();
-        $modules=($user->roles->filter(function($item){
-            return Str::endsWith($item->name, '::admin');
-        }));
-
-        if($modules->count()==1){
-            $panel_name=$modules->first()->name;
-            $module_name=Str::before($panel_name, '::admin');
-            $url='/'.$module_name.'/admin/dashboard';
->>>>>>> c605e84 (redirect for who has only a module)
             redirect($url);
         }
 
@@ -68,11 +41,4 @@ class MainDashboard extends Page
         }
         */
     }
-<<<<<<< HEAD
-=======
-   
-
-    
-    
->>>>>>> c605e84 (redirect for who has only a module)
 }
