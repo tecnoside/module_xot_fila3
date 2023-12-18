97bbaa692ab2750c68a232dfbaf6f8c61787184f
marco76tv <marco.sottana@gmail.com>
2023-12-18 15:31:17 +0100

    Check and fix styling

--- Actions/Filament/GetModulesNavigationItems.php
+++ Actions/Filament/GetModulesNavigationItems.php
@@ -47,7 +47,11 @@ class GetModulesNavigationItems
                 ->sort($config['navigation_sort'] ?? 1)
                 ->visible(static function () use ($role) {
                     $user = Filament::auth()->user();
+<<<<<<< HEAD
                     if (null === $user) {
+=======
+                    if (null == $user) {
+>>>>>>> 6a28ac1 (Check & fix styling)
                         return false;
                     }
 
