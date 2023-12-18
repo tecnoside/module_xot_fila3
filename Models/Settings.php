61ce01c27cc7c4e2f701753ccdbf1494ab719221
marco76tv <marco.sottana@gmail.com>
2023-12-18 15:28:53 +0100

    Update model annotations in Settings.php

--- Models/Settings.php
+++ Models/Settings.php
@@ -17,14 +17,10 @@ use Illuminate\Database\Eloquent\Model;
  * @property mixed                           $payload
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ *
  * @method static Builder|Settings newModelQuery()
  * @method static Builder|Settings newQuery()
  * @method static Builder|Settings query()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 0fa7914 (Update model annotations in Models/Cache.php, Models/CacheLock.php, Models/Feed.php, Models/Profile.php, Models/Session.php, and Models/Settings.php)
  * @method static Builder|Settings whereCreatedAt($value)
  * @method static Builder|Settings whereGroup($value)
  * @method static Builder|Settings whereId($value)
@@ -32,15 +28,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static Builder|Settings whereName($value)
  * @method static Builder|Settings wherePayload($value)
  * @method static Builder|Settings whereUpdatedAt($value)
-<<<<<<< HEAD
  *
-=======
->>>>>>> 36cf382 (up)
-=======
- *
->>>>>>> c34febf (Check & fix styling)
-=======
->>>>>>> 0fa7914 (Update model annotations in Models/Cache.php, Models/CacheLock.php, Models/Feed.php, Models/Profile.php, Models/Session.php, and Models/Settings.php)
  * @mixin \Eloquent
  */
 class Settings extends Model
