1091dd0d7dee7e9501371e31b3f0b67b65f4f596
Malebestia <vair81@gmail.com>
2024-01-09 17:24:34 +0100

    conflict

--- Filament/Pages/MainDashboard.php
+++ Filament/Pages/MainDashboard.php
@@ -23,10 +23,6 @@ class MainDashboard extends Page
     public function mount(): void
     {
         Assert::notNull($user = auth()->user());
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
         $modules = $user->roles->filter(function ($item) {
             return Str::endsWith($item->name, '::admin');
         });
