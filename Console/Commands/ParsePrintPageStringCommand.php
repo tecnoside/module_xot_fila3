731f48b5b27da5fda633296d5d87860bbf40105b
marco76tv <marco.sottana@gmail.com>
2024-01-31 18:10:10 +0100

    up

---
+++ Console/Commands/ParsePrintPageStringCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @see ---
+ */
+
+namespace Modules\Xot\Console\Commands;
+
+use Illuminate\Console\Command;
+use Modules\Xot\Actions\ParsePrintPageStringAction;
+
+class ParsePrintPageStringCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'xot:parse-print-page {str}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = ' esplode';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $str = $this->argument('str');
+        dddx(app(ParsePrintPageStringAction::class)->execute($str));
+    }
+}
