321f7626d1fcf099a005b9da07951ebc5940d111
marco76tv <marco.sottana@gmail.com>
2024-02-08 14:51:17 +0100

    delete include

--- Actions/Export/PdfByHtmlAction.php
+++ Actions/Export/PdfByHtmlAction.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Actions\Export;
 
-// use Modules\Xot\Services\ArrayService;
 use Illuminate\Support\Facades\Storage;
-
-use function Safe\realpath;
-
 use Spatie\QueueableAction\QueueableAction;
 use Spipu\Html2Pdf\Html2Pdf;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -23,10 +19,8 @@ class PdfByHtmlAction
         string $disk = 'cache',
         string $out = 'download'
     ): string|BinaryFileResponse {
-        // include_once realpath(__DIR__.'/../../Services/vendor/autoload.php');
         $html2pdf = new Html2Pdf('L', 'A4', 'it');
         $html2pdf->writeHTML($html);
-        // $filename = 'my_doc.pdf';
         $path = Storage::disk($disk)->path($filename);
         $html2pdf->output($path, 'F');
 
