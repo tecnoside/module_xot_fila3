5b7d580c198d242dde29d77c20bc89d9e9afff26
marco76tv <marco76tv@users.noreply.github.com>
2024-02-13 17:16:12 +0000

    Check & fix styling

--- Datas/PdfData.php
+++ Datas/PdfData.php
@@ -11,9 +11,6 @@ namespace Modules\Xot\Datas;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
-
-use function Safe\realpath;
-
 use Spatie\LaravelData\Data;
 use Spipu\Html2Pdf\Html2Pdf;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
