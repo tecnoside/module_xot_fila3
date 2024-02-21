6ff80651501a2f08d0edf8534989fb01bcb2dd03
marco76tv <marco.sottana@gmail.com>
2024-02-21 19:36:16 +0100

    Refactor PDF export actions for model and table

--- Filament/Actions/Table/PdfAction.php
+++ Filament/Actions/Table/PdfAction.php
@@ -12,24 +12,16 @@ namespace Modules\Xot\Filament\Actions\Table;
 use Filament\Tables\Actions\Action;
 use Modules\Xot\Actions\Export\PdfByModelAction;
 
-<<<<<<< HEAD
 class PdfAction extends Action
 {
     protected function setUp(): void
     {
-=======
-class PdfAction extends Action {
-    protected function setUp(): void {
->>>>>>> cd02905 (up)
         parent::setUp();
 
         $this->translateLabel()
             ->label('')
             ->tooltip('pdf')
-<<<<<<< HEAD
             ->openUrlInNewTab()
-=======
->>>>>>> cd02905 (up)
             // ->icon('heroicon-o-cloud-arrow-down')
             // ->icon('fas-file-excel')
             ->icon('heroicon-o-document-arrow-down')
