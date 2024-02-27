e8c55f680b806999560d1087c1a0fd3ad98aa124
marco xot <marco.sottana@gmail.com>
2024-02-27 14:01:37 +0100

    Add SendMailByRecordAction and SendMailByRecordsAction

---
+++ Actions/Mail/SendMailByRecordsAction.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions\Mail;
+
+use Illuminate\Database\Eloquent\Collection;
+use Spatie\QueueableAction\QueueableAction;
+
+class SendMailByRecordsAction {
+    use QueueableAction;
+
+    /**
+     * Undocumented function.
+     */
+    public function execute(Collection $records,string $mail_class) {
+        foreach ($records as $record) {
+            app(SendMailByRecordAction::class)->execute($record,$mail_class);
+        }
+
+        return true;
+    }
+}
