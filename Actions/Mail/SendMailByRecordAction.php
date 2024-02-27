f5823e37ae06f37e67e9067cc07fed13d02b1fba
marco76tv <marco76tv@users.noreply.github.com>
2024-02-27 13:02:02 +0000

    Check & fix styling

--- Actions/Mail/SendMailByRecordAction.php
+++ Actions/Mail/SendMailByRecordAction.php
@@ -4,44 +4,42 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Actions\Mail;
 
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Spatie\QueueableAction\QueueableAction;
-use Illuminate\Database\Eloquent\Collection;
 
-
-class SendMailByRecordAction {
+class SendMailByRecordAction
+{
     use QueueableAction;
 
     /**
      * Undocumented function.
+     *
      * @return bool
      */
-    public function execute(Model $record,string $mail_class) {
-        
-        if(!$record->canSendEmail()){
+    public function execute(Model $record, string $mail_class)
+    {
+        if (! $record->canSendEmail()) {
             return false;
         }
-        //non uso il gate perche' se no superadmin puo' far casino
-        //if (! Gate::allows('send-mail', $record)) {
+        // non uso il gate perche' se no superadmin puo' far casino
+        // if (! Gate::allows('send-mail', $record)) {
         //    dddx('NO');
-        //}
-        $record_class=get_class($record);
-        $log_class=Str::of($record_class)
+        // }
+        $record_class = get_class($record);
+        $log_class = Str::of($record_class)
             ->before('\Models')
             ->append('\Models\MyLog')
             ->toString();
-        
-        
-        $to=$record->email;
-        $to='marco.sottana@gmail.com';
-        
+
+        $to = $record->email;
+        $to = 'marco.sottana@gmail.com';
+
         Mail::to($to)->send(new $mail_class($record));
-        $record->myLogs()->create(['act'=>'sendMail']);
+        $record->myLogs()->create(['act' => 'sendMail']);
 
         /*
         $log_class::create([
@@ -51,7 +49,7 @@ class SendMailByRecordAction {
             'handle'=>Auth::id(),
         ]);
         */
-       
+
         return true;
     }
 }
