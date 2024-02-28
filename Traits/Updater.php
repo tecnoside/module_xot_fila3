ac3a320e07c1d1c47e21dcfa8a1a5dbec1aa65e2
marco76tv <marco.sottana@gmail.com>
2024-02-28 15:56:41 +0100

    up

--- Traits/Updater.php
+++ Traits/Updater.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Traits;
 
+use Filament\Facades\Filament;
+
 /**
  * Trait Updater.
  * https://dev.to/hasanmn/automatically-update-createdby-and-updatedby-in-laravel-using-bootable-traits-28g9.
@@ -15,44 +17,29 @@ trait Updater
      */
     protected static function bootUpdater(): void
     {
-        /*
-         * During a model create Eloquent will also update the updated_at field so
-         * need to have the updated_by field here as well.
-         */
         static::creating(
             static function ($model): void {
-                // if (auth()->user() instanceof Authenticatable) {
-                $model->created_by = auth()->id();
-                $model->updated_by = auth()->id();
-                // }
+                $model->created_by = Filament::auth()->id();
+                $model->updated_by = Filament::auth()->id();
             }
         );
 
-        /*
-         * updating.
-         */
         static::updating(
             static function ($model): void {
-                $model->updated_by = auth()->id();
+                $model->updated_by = Filament::auth()->id();
             }
         );
-        // -------------------------------------------------------------------------------------
         /*
          * Deleting a model is slightly different than creating or deleting.
          * For deletes we need to save the model first with the deleted_by field
-        */
-        // *
+         */
         static::deleting(
             static function ($model): void {
                 if (\in_array('deleted_by', array_keys($model->attributes), false)) {
-                    $model->deleted_by = auth()->id();
+                    $model->deleted_by = Filament::auth()->id();
                     $model->save();
                 }
             }
         );
-        // */
-        // ----------------------
     }
-
-    // end function boot
 }// end trait Updater
