f009727e80de8834987177b088eda6d534a03e18
marco76tv <marco76tv@users.noreply.github.com>
2024-03-25 11:06:21 +0000

    Check & fix styling

--- Helpers/Helper.php
+++ Helpers/Helper.php
@@ -383,7 +383,7 @@ if (! function_exists('params2ContainerItem')) {
     /**
      * @return array<array>
      */
-    function params2ContainerItem(array $params = null): array
+    function params2ContainerItem(?array $params = null): array
     {
         if (null === $params) {
             // Call to static method current() on an unknown class Route.
@@ -676,7 +676,7 @@ if (! function_exists('url_queries')) {
      *
      * @return string The updated query string
      */
-    function url_queries(array $queries, string $url = null): string
+    function url_queries(array $queries, ?string $url = null): string
     {
         // If a URL isn't supplied, use the current one
         if (! $url) {
