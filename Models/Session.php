e6f3ee13f4da575951ed5de712c2ddb1cb82d236
marco76tv <marco76tv@users.noreply.github.com>
2024-04-17 13:57:57 +0000

    Check & fix styling

--- Models/Session.php
+++ Models/Session.php
@@ -19,6 +19,7 @@ use Illuminate\Database\Eloquent\Builder;
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null                     $created_by
  * @property string|null                     $updated_by
+ *
  * @method static \Modules\Xot\Database\Factories\SessionFactory factory($count = null, $state = [])
  * @method static Builder|Session                                newModelQuery()
  * @method static Builder|Session                                newQuery()
@@ -33,6 +34,7 @@ use Illuminate\Database\Eloquent\Builder;
  * @method static Builder|Session                                whereUpdatedBy($value)
  * @method static Builder|Session                                whereUserAgent($value)
  * @method static Builder|Session                                whereUserId($value)
+ *
  * @property int                             $id
  * @property int|null                        $user_id
  * @property string|null                     $ip_address
@@ -43,6 +45,7 @@ use Illuminate\Database\Eloquent\Builder;
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null                     $created_by
  * @property string|null                     $updated_by
+ *
  * @method static \Modules\Xot\Database\Factories\SessionFactory factory($count = null, $state = [])
  * @method static Builder|Session                                newModelQuery()
  * @method static Builder|Session                                newQuery()
@@ -57,10 +60,13 @@ use Illuminate\Database\Eloquent\Builder;
  * @method static Builder|Session                                whereUpdatedBy($value)
  * @method static Builder|Session                                whereUserAgent($value)
  * @method static Builder|Session                                whereUserId($value)
+ *
  * @property \Illuminate\Support\Carbon|null $deleted_at
- * @property string|null $deleted_by
+ * @property string|null                     $deleted_by
+ *
  * @method static Builder|Session whereDeletedAt($value)
  * @method static Builder|Session whereDeletedBy($value)
+ *
  * @mixin \Eloquent
  */
 class Session extends BaseModel
