986ae8015416f42160692fc0a396d507ca293d96
marco76tv <marco.sottana@gmail.com>
2024-05-06 17:29:50 +0200

    up

---
+++ Actions/Dummy/GetProductsArrayDummyAction.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions\Dummy;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
+use Spatie\QueueableAction\QueueableAction;
+
+class GetProductsArrayDummyAction
+{
+    use QueueableAction;
+
+    /**
+     * Execute the function with the given model class.
+     *
+     * @throws \Exception Generating Factory [factory_class] press [F5] to refresh page [__LINE__][__FILE__]
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function execute(): array
+    {
+        // API
+        $products = Http::get('https://dummyjson.com/products')->json();
+
+        // filtering some attributes
+        $products = Arr::map($products['products'], function ($item) {
+            return Arr::only($item,
+                [
+                    'id',
+                    'title',
+                    'description',
+                    'price',
+                    'rating',
+                    'brand',
+                    'category',
+                    'thumbnail',
+                ]
+            );
+        });
+
+        return $products;
+    }
+}
