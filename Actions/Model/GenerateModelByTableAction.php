db342c652d494abd98f8c3fd47e11fc970f22f01
marco76tv <marco.sottana@gmail.com>
2024-05-15 14:00:26 +0200

    up

---
+++ Actions/Model/GenerateModelByTableAction.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @see https://github.com/krlove/eloquent-model-generator
+ * @see https://github.com/laracademy/generators
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions\Model;
+
+use Spatie\QueueableAction\QueueableAction;
+
+class GenerateModelByTableAction
+{
+    use QueueableAction;
+
+    public function execute(): void
+    {
+        dddx('WIP');
+    }
+}
---
+++ Console/Commands/GenerateModelClassCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class GenerateModelClassCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'xot:generate-model-class {model_class}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new ModelClass.';
+
+    protected function getStub()
+    {
+        return realpath(__DIR__.'/../stubs/model.stub');
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Models';
+    }
+
+    protected function replaceClass($stub, $name)
+    {
+        $class = str_replace($this->getNamespace($name).'\\', '', $name);
+
+        // Do string replacement
+        return str_replace('{{service_name}}', $class, $stub);
+    }
+}
