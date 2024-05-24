5c12433adabb4bc1886a6fbec93c3e195d38c5ec
marco76tv <marco.sottana@gmail.com>
2024-05-24 22:59:38 +0200

    up

--- Services/HtmlService.php
+++ Services/HtmlService.php
@@ -32,7 +32,7 @@ class HtmlService
     {
         // dddx($params);
 
-        include_once __DIR__.'/vendor/autoload.php';
+        //include_once __DIR__.'/vendor/autoload.php';
         // $pdforientation = 'L'; // default;
         // $out = 'show';
         if ('' === $filename) {
