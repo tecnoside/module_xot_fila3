b40ad46d7338dbcc82137fb144fc826afc1d3585
marco76tv <marco.sottana@gmail.com>
2024-06-04 13:31:05 +0200

    style(dashboard.blade.php): update class name to 'fi-dashboard-page' for consistency feat(dashboard.blade.php): add conditional rendering for filtersForm to display only if method exists feat(dashboard.blade.php): pass filters data to widgets component if filters property exists, improve data passing for widgets

--- Resources/views/filament/pages/dashboard.blade.php
+++ Resources/views/filament/pages/dashboard.blade.php
@@ -1,3 +1,7 @@
-<x-filament::page>
+<x-filament-panels::page class="fi-dashboard-page">
+    @if (method_exists($this, 'filtersForm'))
+        {{ $this->filtersForm }}
+    @endif
 
-</x-filament::page>
+    <x-filament-widgets::widgets :columns="$this->getColumns()" :data="[...property_exists($this, 'filters') ? ['filters' => $this->filters] : [], ...$this->getWidgetData()]" :widgets="$this->getVisibleWidgets()" />
+</x-filament-panels::page>
