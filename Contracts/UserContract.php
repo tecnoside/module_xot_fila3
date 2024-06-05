cdb6a78670d8d1c22074a5282ba6ffe9208a2501
marco76tv <marco76tv@users.noreply.github.com>
2024-06-05 10:52:12 +0000

    Check & fix styling

--- Contracts/UserContract.php
+++ Contracts/UserContract.php
@@ -49,8 +49,6 @@ interface UserContract extends CanResetPassword, FilamentUser, HasTeamsContract,
     /**
      * Update the model in the database.
      *
-     * @param mixed $key
-     *
      * @return bool
      */
     /**
@@ -82,7 +80,7 @@ interface UserContract extends CanResetPassword, FilamentUser, HasTeamsContract,
     /**
      * Determine if the model has (one of) the given role(s).
      */
-    public function hasRole(string|int|array|Role|\Illuminate\Support\Collection $roles, string $guard = null): bool;
+    public function hasRole(string|int|array|Role|\Illuminate\Support\Collection $roles, ?string $guard = null): bool;
 
     /**
      * Assign the given role to the model.
