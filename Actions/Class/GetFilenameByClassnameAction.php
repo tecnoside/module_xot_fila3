c94aa9946c921c52a9e899d2a0f99e018054782d
Malebestia <vair81@gmail.com>
2024-06-18 12:31:52 +0200

    .

--- Actions/Class/GetFilenameByClassnameAction.php
+++ Actions/Class/GetFilenameByClassnameAction.php
@@ -12,10 +12,27 @@ class GetFilenameByClassnameAction
 {
     use QueueableAction;
 
+    /**
+     * @param class-string $class_name
+     */
     public function execute(string $class_name): string
     {
+        try {
+            $path = str_replace('\\', '/', $class_name);
+            $path = base_path($path).'.php';
+
+            return $path;
+        } catch (\Throwable $th) {
+            // throw $th;
+        }
+
         $reflector = new \ReflectionClass($class_name);
 
-        return $reflector->getFileName();
+        $filename = $reflector->getFileName();
+
+        if (is_string($filename)) {
+            return $filename;
+        }
+        throw new \Exception('['.__LINE__.']['.__FILE__.']['.$class_name.']');
     }
 }
