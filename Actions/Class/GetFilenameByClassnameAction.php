0f1b38a40a5fa7064b4c907acdf377fcf65840c2
marco76tv <marco76tv@users.noreply.github.com>
2024-06-22 12:25:08 +0000

    Check & fix styling

--- Actions/Class/GetFilenameByClassnameAction.php
+++ Actions/Class/GetFilenameByClassnameAction.php
@@ -6,31 +6,24 @@ namespace Modules\Xot\Actions\Class;
 
 // use Modules\Xot\Services\ArrayService;
 
-use Exception;
 use Spatie\QueueableAction\QueueableAction;
 
 class GetFilenameByClassnameAction
 {
     use QueueableAction;
 
-    /**
-     * @param string $class_name
-     */
     public function execute(string $class_name): string
     {
         $filename = null;
         try {
-            if(class_exists($class_name)) {
+            if (class_exists($class_name)) {
                 $reflector = new \ReflectionClass($class_name);
                 $filename = $reflector->getFileName();
             }
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             $filename = str_replace('\\', '/', $class_name);
             $filename = base_path($filename).'.php';
-
         }
-
-
 
         if (is_string($filename)) {
             return $filename;
