8f7880ac2bad64dbb9c948e7894275a508f2b29e
marco <marco.sottana@gmail.com>
2024-07-02 17:13:44 +0200

    up

--- Models/Extra.php
+++ Models/Extra.php
@@ -17,7 +17,6 @@ use Webmozart\Assert\Assert;
  * @property int|null                                          $model_id
  * @property string|null                                       $model_type
  * @property \Spatie\SchemalessAttributes\SchemalessAttributes $extra_attributes
- *
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel disableCache()
  * @method static \Modules\Xot\Database\Factories\ExtraFactory    factory($count = null, $state = [])
  * @method static Builder|Extra                                   newModelQuery()
@@ -25,14 +24,12 @@ use Webmozart\Assert\Assert;
  * @method static Builder|Extra                                   query()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel withCacheCooldownSeconds(?int $seconds = null)
  * @method static Builder|Extra                                   withExtraAttributes()
- *
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null                     $updated_by
  * @property string|null                     $created_by
  * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property string|null                     $deleted_by
- *
  * @method static Builder|Extra whereCreatedAt($value)
  * @method static Builder|Extra whereCreatedBy($value)
  * @method static Builder|Extra whereDeletedAt($value)
@@ -43,7 +40,6 @@ use Webmozart\Assert\Assert;
  * @method static Builder|Extra whereModelType($value)
  * @method static Builder|Extra whereUpdatedAt($value)
  * @method static Builder|Extra whereUpdatedBy($value)
- *
  * @mixin \Eloquent
  */
 class Extra extends Model
