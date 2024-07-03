eaccc4825f5116e5f1d7b9a0e69c1033070a0c20
marco76tv <marco.sottana@gmail.com>
2024-07-03 14:08:28 +0200

    🔧 (Models): Update database connection to 'xot' for BaseModel, BaseMorphPivot, Extra, and HealthCheckResultHistoryItem models to ensure consistency and use the specified database connection.

--- Models/BaseMorphPivot.php
+++ Models/BaseMorphPivot.php
@@ -30,7 +30,7 @@ abstract class BaseMorphPivot extends MorphPivot
     protected $perPage = 30;
 
     /** @var string */
-    protected $connection = 'xot'; // this will use the specified database connection
+    protected $connection = 'xot';
 
     /** @var array<int, string> */
     protected $appends = [];
