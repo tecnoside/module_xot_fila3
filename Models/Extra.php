1de25fa725baaffe304917e926c03a83c1433ad9
marco76tv <marco76tv@users.noreply.github.com>
2024-07-12 15:03:13 +0000

    Check & fix styling

--- Models/Extra.php
+++ Models/Extra.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property int|null                                          $model_id
  * @property string|null                                       $model_type
  * @property \Spatie\SchemalessAttributes\SchemalessAttributes $extra_attributes
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel disableCache()
  * @method static \Modules\Xot\Database\Factories\ExtraFactory    factory($count = null, $state = [])
  * @method static Builder|Extra                                   newModelQuery()
@@ -21,12 +22,14 @@ use Illuminate\Database\Eloquent\Model;
  * @method static Builder|Extra                                   query()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel withCacheCooldownSeconds(?int $seconds = null)
  * @method static Builder|Extra                                   withExtraAttributes()
+ *
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null                     $updated_by
  * @property string|null                     $created_by
  * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property string|null                     $deleted_by
+ *
  * @method static Builder|Extra whereCreatedAt($value)
  * @method static Builder|Extra whereCreatedBy($value)
  * @method static Builder|Extra whereDeletedAt($value)
@@ -37,6 +40,7 @@ use Illuminate\Database\Eloquent\Model;
  * @method static Builder|Extra whereModelType($value)
  * @method static Builder|Extra whereUpdatedAt($value)
  * @method static Builder|Extra whereUpdatedBy($value)
+ *
  * @mixin \Eloquent
  */
 class Extra extends BaseExtra
