5f25b7e69ef9ae168d7b054a0c528656b473f91a
marco <marco.sottana@gmail.com>
2024-07-12 20:38:34 +0200

    ♻️ (HasExtraTrait.php): Remove duplicate import statements and clean up unused code to improve code readability and maintainability.

--- Models/Traits/HasExtraTrait.php
+++ Models/Traits/HasExtraTrait.php
@@ -4,18 +4,12 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Models\Traits;
 
-use Illuminate\Support\Str;
-use Webmozart\Assert\Assert;
-use Modules\Xot\Models\Extra;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
-<<<<<<< HEAD
 use Illuminate\Support\Str;
 use Modules\Xot\Models\BaseExtra;
 use Modules\Xot\Models\Extra;
 use Webmozart\Assert\Assert;
-=======
->>>>>>> 3c814b8 (.)
 
 trait HasExtraTrait
 {
