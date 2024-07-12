b043de8453a1bb6a15dafc57e996c6a0455c3870
marco <marco.sottana@gmail.com>
2024-07-12 17:57:12 +0200

    .

--- Models/Traits/HasExtraTrait.php
+++ Models/Traits/HasExtraTrait.php
@@ -9,13 +9,7 @@ use Webmozart\Assert\Assert;
 use Modules\Xot\Models\Extra;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
-<<<<<<< HEAD
-use Illuminate\Support\Str;
 use Modules\Xot\Models\BaseExtra;
-use Modules\Xot\Models\Extra;
-use Webmozart\Assert\Assert;
-=======
->>>>>>> 3de937b (.)
 
 trait HasExtraTrait
 {
