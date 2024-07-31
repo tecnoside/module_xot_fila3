6d3adaaf969002c4c6b7c2c3e8d63af566cd69a2
marco76tv <marco.sottana@gmail.com>
2024-07-31 10:18:34 +0200

    🔧 (Module.php): Remove unnecessary conflict markers from the file

--- Models/Module.php
+++ Models/Module.php
@@ -10,14 +10,21 @@ use Nwidart\Modules\Facades\Module as ModuleFacade;
 use Sushi\Sushi;
 
 /**
+<<<<<<< HEAD
  *
  *
+=======
+>>>>>>> 0275d76 (🔧 (XotData.php): Remove unnecessary conflict markers from the file)
  * @property int         $id
  * @property string|null $name
  * @property string|null $description
  * @property bool|null   $status
  * @property int|null    $priority
  * @property string|null $path
+<<<<<<< HEAD
+=======
+ *
+>>>>>>> 0275d76 (🔧 (XotData.php): Remove unnecessary conflict markers from the file)
  * @method static \Illuminate\Database\Eloquent\Builder|Module newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Module newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Module query()
@@ -27,6 +34,10 @@ use Sushi\Sushi;
  * @method static \Illuminate\Database\Eloquent\Builder|Module wherePath($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Module wherePriority($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Module whereStatus($value)
+<<<<<<< HEAD
+=======
+ *
+>>>>>>> 0275d76 (🔧 (XotData.php): Remove unnecessary conflict markers from the file)
  * @mixin \Eloquent
  */
 class Module extends Model
