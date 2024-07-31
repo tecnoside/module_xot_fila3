694c693aea18461e03e96494ae9c9f1008f291be
marco76tv <marco.sottana@gmail.com>
2024-07-31 11:34:32 +0200

    🔧 (UserContract.php): resolve conflict and remove duplicate imports for Role and HasTeamsContract 🔧 (Extra.php): resolve conflict and remove duplicate properties and methods in the Extra model

--- Contracts/UserContract.php
+++ Contracts/UserContract.php
@@ -13,13 +13,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Laravel\Passport\Token;
-<<<<<<< HEAD
 use Spatie\Permission\Contracts\Role;
 use Modules\User\Contracts\HasTeamsContract;
-=======
-use Modules\User\Contracts\HasTeamsContract;
-use Spatie\Permission\Contracts\Role;
->>>>>>> 76f06a8 (Check & fix styling)
 
 // use Filament\Models\Contracts\HasTenants;
 
