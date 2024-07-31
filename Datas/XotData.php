768f6b3643235e63ed1dde7a4e44feb22df56739
marco76tv <marco.sottana@gmail.com>
2024-07-31 10:19:33 +0200

    🔧 (XotData.php): remove unnecessary merge conflict markers from the file

--- Datas/XotData.php
+++ Datas/XotData.php
@@ -107,10 +107,7 @@ class XotData extends Data implements Wireable
 
         return $class;
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> 3c80fec (up)
     /**
      * @return class-string<Model&TeamContract>
      */
