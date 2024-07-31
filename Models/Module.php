b1c228ed37fcc1bd47e7f1534a17b936e0f63d39
marco76tv <marco.sottana@gmail.com>
2024-07-31 10:19:00 +0200

    🔧 (Module.php): Remove unnecessary conflict markers and empty lines for better code readability and cleanliness

--- Models/Module.php
+++ Models/Module.php
@@ -10,29 +10,14 @@ use Nwidart\Modules\Facades\Module as ModuleFacade;
 use Sushi\Sushi;
 
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
  *
  *
-=======
->>>>>>> 0275d76 (🔧 (XotData.php): Remove unnecessary conflict markers from the file)
-=======
- * 
- *
->>>>>>> 4a6b3ad (📝 (Models): Remove unnecessary empty lines and comments for better code readability and cleanliness.)
  * @property int         $id
  * @property string|null $name
  * @property string|null $description
  * @property bool|null   $status
  * @property int|null    $priority
  * @property string|null $path
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
- *
->>>>>>> 0275d76 (🔧 (XotData.php): Remove unnecessary conflict markers from the file)
-=======
->>>>>>> 4a6b3ad (📝 (Models): Remove unnecessary empty lines and comments for better code readability and cleanliness.)
  * @method static \Illuminate\Database\Eloquent\Builder|Module newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Module newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Module query()
@@ -42,13 +27,6 @@ use Sushi\Sushi;
  * @method static \Illuminate\Database\Eloquent\Builder|Module wherePath($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Module wherePriority($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Module whereStatus($value)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
- *
->>>>>>> 0275d76 (🔧 (XotData.php): Remove unnecessary conflict markers from the file)
-=======
->>>>>>> 4a6b3ad (📝 (Models): Remove unnecessary empty lines and comments for better code readability and cleanliness.)
  * @mixin \Eloquent
  */
 class Module extends Model
