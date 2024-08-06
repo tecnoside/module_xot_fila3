a62932c2be630f75fc42a717b643a66dc0b2bd4b
marco76tv <marco.sottana@gmail.com>
2024-08-06 09:18:14 +0200

    🔧 (EnvPage.php): remove commented out code related to Players form component to clean up the codebase and improve readability

--- Filament/Pages/EnvPage.php
+++ Filament/Pages/EnvPage.php
@@ -15,43 +15,4 @@ class EnvPage extends Page
 
     protected static string $view = 'xot::filament.pages.dashboard';
 
-<<<<<<< HEAD
-=======
-    protected function getFormSchema(): array
-    {
-        return [
-            TextInput::make('app_url')
-                ->label('URL')
-                ->placeholder('http://localhost')
-                ->helperText('Required for file uploads and other internal configs')
-                ->required(),
-            Toggle::make('debugbar_enabled')
-                ->label('Is Enabled')
-                ->helperText('Enable/Disable debug mode to help debug errors'),
-        ];
-    }
-
-    public function form(Forms\Form $form): Forms\Form
-    {
-        return $form
-            ->schema($this->getFormSchema())
-            ->columns(2)
-            ->statePath('data');
-    }
-
-    public function submit()
-    {
-        $res = EnvData::make()->update($this->data);
-        Notification::make()
-            ->title('Saved successfully')
-            ->success()
-            ->send();
-        /*
-        dddx([
-            'data' => $this->data,
-            // 'data1' => $this->form->getState(),
-        ]);
-        */
-    }
->>>>>>> 7d83ae0 (🔧 (EnvPage.php): remove commented out code related to Players form component to clean up the codebase and improve readability)
 }
