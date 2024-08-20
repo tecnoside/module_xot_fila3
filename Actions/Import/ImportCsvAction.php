460da49f2a487a38ba558c4621ea6a879375b45a
marco76tv <marco.sottana@gmail.com>
2024-08-20 11:49:21 +0200

    rebase 30

--- Actions/Import/ImportCsvAction.php
+++ Actions/Import/ImportCsvAction.php
@@ -12,17 +12,27 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Spatie\QueueableAction\QueueableAction;
 
+<<<<<<< HEAD
 use function Safe\ini_set;
 use function Safe\preg_replace;
 
+=======
+>>>>>>> f3b50b6 (up)
 class ImportCsvAction
 {
     use QueueableAction;
 
+<<<<<<< HEAD
     public function execute(string $disk, string $filename, string $db, string $tbl): void
     {
         ini_set('max_execution_time', '0');
         ini_set('memory_limit', '-1'); // '512M'
+=======
+    public function execute(string $disk, string $filename, string $db, string $tbl)
+    {
+        ini_set('max_execution_time', 0);
+        ini_set('memory_limit', -1); // '512M'
+>>>>>>> f3b50b6 (up)
 
         $storage = Storage::disk($disk);
         $path = $storage->path($filename);
@@ -43,7 +53,11 @@ class ImportCsvAction
         foreach ($columns as $item) {
             $fieldname = $this->fixFieldName($item['name']);
             // if ('numero' === $item['tipo'] && $item['dec'] > 0) {
+<<<<<<< HEAD
             if ($item['type_name'] === 'decimal') {
+=======
+            if ('decimal' === $item['type_name']) {
+>>>>>>> f3b50b6 (up)
                 $fieldname = '@'.$fieldname;
             }
 
@@ -57,7 +71,11 @@ class ImportCsvAction
         $sql_replace = [];
         foreach ($columns as $item) {
             // if ('numero' === $item['tipo'] && $item['dec'] > 0) {
+<<<<<<< HEAD
             if ($item['type_name'] === 'decimal') {
+=======
+            if ('decimal' === $item['type_name']) {
+>>>>>>> f3b50b6 (up)
                 $fieldname = $this->fixFieldName($item['name']);
                 $sql_replace[] = $fieldname.' = REPLACE(@'.$fieldname.',"," , ".")';
             }
@@ -68,7 +86,10 @@ class ImportCsvAction
             $sql = $sql.'SET '.$sql_replace.';';
         }
 
+<<<<<<< HEAD
         $pdo->exec('SET GLOBAL local_infile=1;');
+=======
+>>>>>>> f3b50b6 (up)
         // echo '<pre>'.htmlspecialchars($sql).'</pre>';
         $n_rows = $pdo->exec($sql);
         // dddx($n_rows);
@@ -83,7 +104,11 @@ class ImportCsvAction
     public function fixFieldName(string $str): string
     {
         $str = trim($str);
+<<<<<<< HEAD
         if ($str === 'desc') {
+=======
+        if ('desc' === $str) {
+>>>>>>> f3b50b6 (up)
             return 'desc1';
         } // descrizione;
         // preg_match_all(, subject, matches)
