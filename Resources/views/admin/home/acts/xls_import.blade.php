2f3a9c02b65bc73bbc8a5b06ec8824573ea182a4
marco <marco.sottana@gmail.com>
2024-08-26 15:42:07 +0200

    up

--- Resources/views/admin/home/acts/xls_import.blade.php
+++ Resources/views/admin/home/acts/xls_import.blade.php
@@ -1,17 +1,4 @@
 @extends('adm_theme::layouts.app')
 @section('content')
-    {{-- <div class="card bg-light mt-3">
-        <div class="card-header">
-            Import Excel to Database
-        </div>
-        <div class="card-body">
-            <form action="{{ Request::fullUrl() }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                <input type="file" name="file" class="form-control">
-                <br>
-                <button class="btn btn-success">Next !</button>
-            </form>
-        </div>
-    </div> --}}
     <livewire:import.xls.model modelClass="\Modules\User\Models\User" />
 @endsection
