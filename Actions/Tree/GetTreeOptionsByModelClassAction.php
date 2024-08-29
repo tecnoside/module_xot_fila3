e335ef270303f9c3875558dfee986448ec6b786f
marco <marco.sottana@gmail.com>
2024-08-29 12:10:13 +0200

    ✨ (GetTreeOptionsByModelClassAction.php): Add new action class to generate tree options for a given model class based on recursive relationships 📝 (HasRecursiveRelationshipsContract.php): Add new methods and properties to define recursive relationships for models implementing this contract 🔧 (Cache.php, CacheLock.php): Remove redundant comments and properties from Cache and CacheLock models to improve code readability and maintainability
    📝 (Models): Remove unnecessary empty lines and comments for better code readability and maintainability.

---
+++ Actions/Tree/GetTreeOptionsByModelClassAction.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Actions\Tree;
+
+use Illuminate\Support\Str;
+use Modules\Xot\Contracts\HasRecursiveRelationshipsContract;
+use Spatie\QueueableAction\QueueableAction;
+
+class GetTreeOptionsByModelClassAction
+{
+    use QueueableAction;
+
+    public array $options = [];
+
+    /**
+     * Summary of execute.
+     *
+     * @param class-string $class
+     */
+    public function execute(string $class): array
+    {
+        $rows = $class::tree()->get()->toTree();
+
+        foreach ($rows as $row) {
+            $this->options[$row->id] = $row->name;
+            $this->parse($row);
+        }
+
+        return $this->options;
+    }
+
+    public function parse(HasRecursiveRelationshipsContract $model): void
+    {
+        foreach ($model->children as $child) {
+            $this->options[$child->id] = Str::repeat('---', $child->depth).'   '.$child->name;
+        }
+    }
+}
