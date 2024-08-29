17a36a0e9567372cfeaffa258a59366442bf0b98
marco <marco.sottana@gmail.com>
2024-08-29 11:17:26 +0200

    up

---
+++ Contracts/HasRecursiveRelationshipsContract.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Xot\Contracts;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Modules\Xot\Contracts\HasRecursiveRelationshipsContract.
+ *
+ * @phpstan-require-extends Model
+ *
+ * @mixin \Eloquent
+ */
+interface HasRecursiveRelationshipsContract
+{
+}
