67662c719a3f041c5e748a94fa2be875b40c879f
marco76tv <marco76tv@users.noreply.github.com>
2024-09-13 19:39:23 +0000

    Check & fix styling

--- Contracts/PivotContract.php
+++ Contracts/PivotContract.php
@@ -9,7 +9,8 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Modules\Xot\Contracts\PivotContract.
  *
-<<<<<<< HEAD
+ * <<<<<<< HEAD
+ *
  * @property string|null                         $title
  * @property string|null                         $subtitle
  * @property string|null                         $price
@@ -17,13 +18,13 @@ use Illuminate\Database\Eloquent\Model;
  * @property int|null                            $status
  * @property Collection|array<ProductContract>   $products
  * @property Collection|array<ChangeCatContract> $changeCats
-=======
- * @property string|null $title
- * @property string|null $subtitle
- * @property string|null $price
- * @property string|null $price_currency
- * @property int|null    $status
->>>>>>> 0ffa67dd1155c8759a2c13bebc9f017bd62111e3
+ *                                                               =======
+ * @property string|null                         $title
+ * @property string|null                         $subtitle
+ * @property string|null                         $price
+ * @property string|null                         $price_currency
+ * @property int|null                            $status
+ *                                                               >>>>>>> 0ffa67dd1155c8759a2c13bebc9f017bd62111e3
  *
  * @method mixed update($params)
  *
