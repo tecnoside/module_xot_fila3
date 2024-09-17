e43534dbea4c572da0470be23d2e50a1e9f2c3e1
marco <marco.sottana@gmail.com>
2024-09-17 17:38:14 +0200

    up

--- Models/CacheLock.php
+++ Models/CacheLock.php
@@ -12,7 +12,6 @@ use Illuminate\Database\Eloquent\Builder;
  * @property string $key
  * @property string $owner
  * @property int    $expiration
- *
  * @method static \Modules\Xot\Database\Factories\CacheLockFactory factory($count = null, $state = [])
  * @method static Builder|CacheLock                                newModelQuery()
  * @method static Builder|CacheLock                                newQuery()
@@ -20,9 +19,7 @@ use Illuminate\Database\Eloquent\Builder;
  * @method static Builder|CacheLock                                whereExpiration($value)
  * @method static Builder|CacheLock                                whereKey($value)
  * @method static Builder|CacheLock                                whereOwner($value)
- *
  * @property int $expiration
- *
  * @method static \Modules\Xot\Database\Factories\CacheLockFactory factory($count = null, $state = [])
  * @method static Builder|CacheLock                                newModelQuery()
  * @method static Builder|CacheLock                                newQuery()
@@ -30,10 +27,8 @@ use Illuminate\Database\Eloquent\Builder;
  * @method static Builder|CacheLock                                whereExpiration($value)
  * @method static Builder|CacheLock                                whereKey($value)
  * @method static Builder|CacheLock                                whereOwner($value)
- *
  * @property \Modules\Xot\Contracts\ProfileContract|null $creator
  * @property \Modules\Xot\Contracts\ProfileContract|null $updater
- *
  * @mixin \Eloquent
  */
 class CacheLock extends BaseModel
