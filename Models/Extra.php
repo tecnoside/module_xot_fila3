bbb5643e1de54c1c009335fb6a93314460d3e27b
marco <marco.sottana@gmail.com>
2024-09-17 18:41:36 +0200

    up

--- Models/Extra.php
+++ Models/Extra.php
@@ -13,7 +13,6 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
  * @property int|null                                          $model_id
  * @property string|null                                       $model_type
  * @property \Spatie\SchemalessAttributes\SchemalessAttributes $extra_attributes
- *
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel       disableCache()
  * @method static \Modules\Xot\Database\Factories\ExtraFactory          factory($count = null, $state = [])
  * @method static \Illuminate\Contracts\Database\Eloquent\Builder|Extra newModelQuery()
@@ -21,14 +20,12 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
  * @method static Builder|Extra                                         query()
  * @method static \Illuminate\Database\Eloquent\Builder|BaseModel       withCacheCooldownSeconds(?int $seconds = null)
  * @method static Builder|Extra                                         withExtraAttributes()
- *
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property string|null                     $updated_by
  * @property string|null                     $created_by
  * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property string|null                     $deleted_by
- *
  * @method static Builder|Extra whereCreatedAt($value)
  * @method static Builder|Extra whereCreatedBy($value)
  * @method static Builder|Extra whereDeletedAt($value)
@@ -39,7 +36,6 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
  * @method static Builder|Extra whereModelType($value)
  * @method static Builder|Extra whereUpdatedAt($value)
  * @method static Builder|Extra whereUpdatedBy($value)
- *
  * @mixin \Eloquent
  */
 class Extra extends BaseExtra
