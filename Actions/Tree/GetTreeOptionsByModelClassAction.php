5976cb75b8a3d98c2e0168e791d9a379af839fb2
marco76tv <marco.sottana@gmail.com>
2024-09-18 11:02:10 +0200

    up

--- Actions/Tree/GetTreeOptionsByModelClassAction.php
+++ Actions/Tree/GetTreeOptionsByModelClassAction.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Modules\Xot\Actions\Tree;
 
 use Illuminate\Support\Str;
-use Modules\Xot\Contracts\HasRecursiveRelationshipsContract;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\QueueableAction\QueueableAction;
+use Modules\Xot\Contracts\HasRecursiveRelationshipsContract;
 
 class GetTreeOptionsByModelClassAction
 {
@@ -19,12 +20,16 @@ class GetTreeOptionsByModelClassAction
      *
      * @param class-string $class
      */
-    public function execute(string $class): array
+    public function execute(string $class, Model|callable|null $where): array
     {
-        $rows = $class::tree()->get()->toTree();
+        if($where == null) {
+            $rows = $class::tree()->get()->toTree();
+        } else {
+            $rows = $class::treeOf($where)->get()->toTree();
+        }
 
         foreach ($rows as $row) {
-            $this->options[$row->id] = $row->name;
+            $this->options[$row->getKey()] = $row->getLabel();
             $this->parse($row);
         }
 
@@ -34,7 +39,7 @@ class GetTreeOptionsByModelClassAction
     public function parse(HasRecursiveRelationshipsContract $model): void
     {
         foreach ($model->children as $child) {
-            $this->options[$child->id] = Str::repeat('---', $child->depth).'   '.$child->name;
+            $this->options[$child->getKey()] = Str::repeat('---', $child->depth).'   '.$child->getLabel();
         }
     }
 }
