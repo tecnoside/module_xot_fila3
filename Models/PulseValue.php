cfc1e66a8c50607ac9cfde14a4efd078944ab74e
marco76tv <marco76tv@users.noreply.github.com>
2024-10-18 10:03:44 +0000

    Check & fix styling

--- Models/PulseValue.php
+++ Models/PulseValue.php
@@ -5,26 +5,28 @@ declare(strict_types=1);
 namespace Modules\Xot\Models;
 
 /**
- * 
- *
  * @property \Modules\Xot\Contracts\ProfileContract|null $creator
  * @property \Modules\Xot\Contracts\ProfileContract|null $updater
+ *
  * @method static \Modules\Xot\Database\Factories\PulseValueFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue  newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue  newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue  query()
- * @property int $id
- * @property int $timestamp
- * @property string $type
- * @property string $key
+ *
+ * @property int         $id
+ * @property int         $timestamp
+ * @property string      $type
+ * @property string      $key
  * @property string|null $key_hash
- * @property string $value
+ * @property string      $value
+ *
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue whereKey($value)
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue whereKeyHash($value)
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue whereTimestamp($value)
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue whereType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|PulseValue whereValue($value)
+ *
  * @mixin \Eloquent
  */
 class PulseValue extends BaseModel
