7ce428ac73545dd8ac72a910c5f1eed4c69b40e0
marco <marco.sottana@gmail.com>
2024-10-21 09:24:01 +0200

    🐛 (PivotContract.php): fix incorrect property type and name in pivot contract ✨ (PivotContract.php): add support for additional properties and methods to the pivot contract

--- Contracts/PivotContract.php
+++ Contracts/PivotContract.php
@@ -9,9 +9,29 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Modules\Xot\Contracts\PivotContract.
  *
+<<<<<<< HEAD
  * @property string|null $title
  * @property string|null $subtitle
  * @property int|null    $status
+=======
+ * @property string|null                         $title
+ * @property string|null                         $subtitle
+ * @property string|null                         $price
+ * @property string|null                         $price_currency
+ * @property int|null                            $status
+ * @property Collection|array<ProductContract>   $products
+ * @property Collection|array<ChangeCatContract> $changeCats
+ * @property string|null                         $title
+ * @property string|null                         $subtitle
+ * @property string|null                         $price
+ * @property string|null                         $price_currency
+ * @property int|null                            $status
+ * @property string|null                         $title
+ * @property string|null                         $subtitle
+ * @property string|null                         $price
+ * @property string|null                         $price_currency
+ * @property int|null                            $status
+>>>>>>> 47a2e603 (up)
  *
  * @method mixed update($params)
  *
