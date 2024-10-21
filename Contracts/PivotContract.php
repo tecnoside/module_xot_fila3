15b86ce3f6c765f7e94fbeb3b03a7bb979aff5e3
marco <marco.sottana@gmail.com>
2024-10-21 09:23:28 +0200

    🐛 (PivotContract.php): fix typo in property name "title" and add missing properties "subtitle", "price", "price_currency", and "status" ✨ (PivotContract.php): add support for "products" and "changeCats" collections

--- Contracts/PivotContract.php
+++ Contracts/PivotContract.php
@@ -9,33 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Modules\Xot\Contracts\PivotContract.
  *
-<<<<<<< HEAD
  * @property string|null $title
  * @property string|null $subtitle
  * @property int|null    $status
-=======
- * <<<<<<< HEAD
- *
- * @property string|null                         $title
- * @property string|null                         $subtitle
- * @property string|null                         $price
- * @property string|null                         $price_currency
- * @property int|null                            $status
- * @property Collection|array<ProductContract>   $products
- * @property Collection|array<ChangeCatContract> $changeCats
- *                                                               =======
- * @property string|null                         $title
- * @property string|null                         $subtitle
- * @property string|null                         $price
- * @property string|null                         $price_currency
- * @property int|null                            $status
- * @property string|null                         $title
- * @property string|null                         $subtitle
- * @property string|null                         $price
- * @property string|null                         $price_currency
- * @property int|null                            $status
- *                                                               >>>>>>> 9a1e719aa93e06137cb8175cb55e169573197018
->>>>>>> 104a70d8 (Check & fix styling)
  *
  * @method mixed update($params)
  *
