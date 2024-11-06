736797d32aa4e1261c1863a9b58421a0a3d44ea2
marco76tv <marco.sottana@gmail.com>
2024-11-06 17:25:39 +0100

    up

--- Filament/Pages/XotBaseListRecords.php
+++ Filament/Pages/XotBaseListRecords.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Pages;
 
-use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Str;
 use Modules\UI\Enums\TableLayoutEnum;
-use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
+use Filament\Resources\Pages\ListRecords;
+use Modules\Xot\Filament\Traits\TransTrait;
+use Modules\Xot\Filament\Traits\HasXotTable;
 use Modules\Xot\Filament\Actions\Header\ExportXlsAction;
+use Modules\UI\Filament\Actions\Table\TableLayoutToggleTableAction;
 
 /**
  * Undocumented class.
@@ -16,8 +19,18 @@ use Modules\Xot\Filament\Actions\Header\ExportXlsAction;
  */
 abstract class XotBaseListRecords extends ListRecords
 {
+    use TransTrait;
+    
+    use HasXotTable;
+    
+    
+    public static function getResource():string {
+        $resource=Str::of(static::class)->before('\Pages\\')->toString();
+        return $resource;
+    }
+    
+    /*
     public TableLayoutEnum $layoutView = TableLayoutEnum::LIST;
-
     protected function getTableHeaderActions(): array
     {
         return [
@@ -27,17 +40,18 @@ abstract class XotBaseListRecords extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        /*
+        
         $anno=Arr::get($this->tableFilters,'anno.value');
         return [
             Actions\CreateAction::make(),
             app(CopyFromLastYearButton::class)->execute(Assenze::class,'anno',$anno),
         ];
-        */
+        
         return [
             // app(ExportButton::class)->execute(),
             // app(ImportButton::class)->execute(),
             ExportXlsAction::make(),
         ];
     }
+    */
 }
